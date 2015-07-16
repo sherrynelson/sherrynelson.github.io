393a78a3904f9ecb903d9b53f06935b13066aa3b
Sherry <sherry.nelson@gmail.com>
2015-07-16 00:29:52 -0400

    added controls for theta and recursion steps

--- Progs/twist.js
+++ Progs/twist.js
@@ -9,6 +9,7 @@ var gl;
 var points = [];
 
 var NumTimesToSubdivide = 5;
+var theta = 0.0;
 
 window.onload = function init()
 {
@@ -17,8 +18,7 @@ window.onload = function init()
     gl = WebGLUtils.setupWebGL( canvas );
     if ( !gl ) { alert( "WebGL isn't available" ); }
 
-    //
-    //  Initialize our data for the Sierpinski Gasket
+ //  Initialize our data for the Sierpinski Gasket
     //
 
     // First, initialize the corners of our gasket with three points.
@@ -31,6 +31,8 @@ window.onload = function init()
 
     divideTriangle( vertices[0], vertices[1], vertices[2],
                     NumTimesToSubdivide);
+
+
 
     //
     //  Configure WebGL
@@ -47,6 +49,7 @@ window.onload = function init()
 
     var bufferId = gl.createBuffer();
     gl.bindBuffer( gl.ARRAY_BUFFER, bufferId );
+   // gl.bufferData( gl.ARRAY_BUFFER, 8*Math.pow(3,6), gl.STATIC_DRAW );
     gl.bufferData( gl.ARRAY_BUFFER, flatten(points), gl.STATIC_DRAW );
 
     // Associate out shader variables with our data buffer
@@ -55,7 +58,17 @@ window.onload = function init()
     gl.vertexAttribPointer( vPosition, 2, gl.FLOAT, false, 0, 0 );
     gl.enableVertexAttribArray( vPosition );
 
-    render();
+   document.getElementById("theta_slider").onchange = function() {
+	theta = document.getElementById("theta_slider").value * Math.PI/180;;
+	render();
+   };
+   document.getElementById("subdiv_slider").onchange = function() {
+	NumTimesToSubdivide = document.getElementById("subdiv_slider").value;
+        render();
+    };
+
+    gl.clear( gl.COLOR_BUFFER_BIT );
+    gl.drawArrays( gl.TRIANGLES, 0, points.length );
 };
 
 function triangle( a, b, c )
@@ -70,7 +83,7 @@ function twistNscale(vec){
 	var x = vec[0];
 	var y = vec[1];
 	//set theta to an  arbitrary angle (45 degrees) to rotate triangle:
-	var theta = 45 * Math.PI/180;
+//	var theta = 45 * Math.PI/180;
 	//d= distance from origin
 	var d = Math.sqrt(Math.pow(x, 2) + Math.pow(y, 2));
 	//the rotated x position of this point:
@@ -118,6 +131,21 @@ function divideTriangle( a, b, c, count )
 
 function render()
 {
+   var vertices = [
+        vec2( -1, -1 ),
+        vec2(  0,  1 ),
+        vec2(  1, -1 )
+    ];
+    points = [];
+    divideTriangle( vertices[0], vertices[1], vertices[2],
+                    NumTimesToSubdivide);
+
+    gl.bufferSubData(gl.ARRAY_BUFFER, 0, flatten(points));
+
     gl.clear( gl.COLOR_BUFFER_BIT );
     gl.drawArrays( gl.TRIANGLES, 0, points.length );
+    points = [];
 }
+
+
+
